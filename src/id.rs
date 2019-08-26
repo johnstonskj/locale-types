8087ab71859cfaf3742303fa63b287bfe509685e
Simon Johnston <johnstonskj@gmail.com>
2019-08-26 09:45:59 -0700

    added docs to LocaleIdentifier

--- src/id.rs
+++ src/id.rs
@@ -8,30 +8,41 @@ use std::fmt::Display;
 // Public Types
 // ------------------------------------------------------------------------------------------------
 
+/// Common trait for different kinds of locale identifier.
 pub trait LocaleIdentifier
 where
     Self: Sized,
 {
+    /// Construct a new identifier with the given language code only.
     fn new(language_code: String) -> Self;
 
+    /// Return a new identifier based on `self` with a new language code.
     fn with_language(&self, language_code: String) -> Self;
 
+    /// Return a new identifier based on `self` with a new territory code.
     fn with_territory(&self, territory: String) -> Self;
 
+    /// Return a new identifier based on `self` with a new code_set code.
     fn with_code_set(&self, code_set: String) -> Self;
 
+    /// Return a new identifier based on `self` with a new modifier string.
     fn with_modifier(&self, modifier: String) -> Self;
 
+    /// Return a new identifier based on `self` with a new modifier string.
     fn with_modifiers<K, V>(&self, modifiers: HashMap<K, V>) -> Self
     where
         K: Display,
         V: Display;
 
+    /// Return the current language code.
     fn language_code(&self) -> String;
 
+    /// Return the current territory code.
     fn territory(&self) -> Option<String>;
 
+    /// Return the current code set code.
     fn code_set(&self) -> Option<String>;
 
+    /// Return the current modifier string.
     fn modifier(&self) -> Option<String>;
 }
